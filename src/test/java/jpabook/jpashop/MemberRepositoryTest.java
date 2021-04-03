095051be0def44c6af441d712aa10d3be2b09309
keepbang <rlqud1125@naver.com>
2021-04-03 17:30:27 +0900

    도메인 작성

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -11,23 +11,6 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class MemberRepositoryTest {
 
-    @Autowired
-    MemberRepository memberRepository;
 
-    @Test
-    @Transactional
-    @Rollback(true) //테스트 종료 후 데이터를 롤백하지 않음
-    public void testMember() throws Exception{
-        Member member = new Member();
-        member.setUsername("user1");
-
-        Long saveId = memberRepository.save(member);
-        Member findMember = memberRepository.find(saveId);
-
-        assertEquals(findMember.getId(), member.getId());
-        assertEquals(findMember.getUsername(),member.getUsername());
-        assertEquals(findMember,member);
-
-    }
 
 }
