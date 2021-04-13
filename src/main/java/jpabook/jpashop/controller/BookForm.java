08d797d7181d211b580a61fe904dcbe4bcc11999
keepbang <rlqud1125@naver.com>
2021-04-13 22:36:29 +0900

    상품 등록,목록 조회

---
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm extends ItemForm{
+
+    private String author;
+    private String isbn;
+
+}
---
+++ src/main/java/jpabook/jpashop/controller/ItemForm.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ItemForm {
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    private String itemType;
+
+    private String author;
+    private String isbn;
+
+    private String artist;
+    private String etc;
+
+    private String director;
+    private String actor;
+}
